4141f99690ed29735496860ffc2f80909e9ead16
draco2134435 <ms3990@g.rit.edu>
2016-02-03 10:50:45 -0500

    Commit 3
    Added interactable npc that changes non battle scene to shop scene.

---
+++ ProjectFantasia/Assets/Scripts/MerchantShop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MerchantShop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerStay2D(Collider2D other)
+    {
+       
+        if (Input.GetKey(KeyCode.E) == true)
+        {
+
+            SceneManager.LoadScene("ShopScene");
+            
+        }
+
+    }
+}
