64f590883fff52a8e6d69b9753980fb547efde5f
draco2134435 <ms3990@g.rit.edu>
2016-02-25 21:57:16 -0500

    commit 4
    Added inventory system

---
+++ ProjectFantasia/Assets/Scripts/Inventory.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Inventory : MonoBehaviour {
+    public int slotsx, slotsy;
+    public List<Item> inventory = new List<Item>();
+    public List<Item> slots = new List<Item>();
+    private ItemDatabase database;
+    public bool showInventory;
+
+    void Start()
+    {
+        for (int i = 0; i< (slotsx* slotsy); i++)
+        {
+            slots.Add(new Item());
+            inventory.Add(new Item());
+        }
+        database = GameObject.Find("ItemDatabase").GetComponent<ItemDatabase>();
+        inventory[0] = (database.items[0]);
+        inventory[1] = database.items[1];
+        
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory"))
+        {
+            showInventory = !showInventory;
+        }
+    }
+    void OnGUI()
+    {
+        if(showInventory)
+        {
+            DrawInventory();
+        }
+        
+    }
+    void DrawInventory()
+    {
+        int i = 0;
+        for (int x = 0; x<slotsx ;x++)
+        {
+            for (int y = 0; y< slotsy; y++)
+            {
+                Rect slotRect = new Rect(x * 60, y * 60, 50, 50);
+                GUI.Box(slotRect, "");
+                slots[i] = inventory[i];
+                if(slots[i].itemName != null)
+                {
+                    GUI.DrawTexture(slotRect,slots[i].itemIcon);
+                }
+                i++;
+            }
+        }
+    }
+}
---
+++ ProjectFantasia/Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ItemDatabase : MonoBehaviour {
+    public List<Item> items = new List<Item>();
+    void Start()
+    {
+        items.Add(new Item("grapes", 0, "tastes good", 0, 0, Item.ItemType.Consumable));
+        items.Add(new Item("Steel Sword", 0, "Starter Wep", 2, 1, Item.ItemType.Weapon));
+    }
+}
